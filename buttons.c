eabaf490327a1a0715f8c9358c240e35f025b786
Marcus Pries <email@marcus-pries.de>
2014-10-06 23:54:02 +0200

    Kommunikation zwischen dem Thread und der G_Main_Loop auf GAsyncQueue umgestellt

--- buttons.c
+++ buttons.c
@@ -5,10 +5,18 @@ struct GUI_ELEMENTE *gui;
 gint eingabe;
 gint anzahl_zeilen;
 GtkListStore *store;
+GAsyncQueue *queue;
+
+};
+
+struct CATCHER{
+GThread *thread_id;
+GAsyncQueue *queue;
+struct REFRESH *refresh;
 };
 
 //lokale Variablen
-gboolean thread_status = FALSE;
+
 
 //lokale funktionen
 gpointer *thread_refresh_button (gpointer data);
@@ -19,12 +27,17 @@ void button_refresh_clicked(GtkWidget *widget, gpointer data)
 {
 	//Variablen erzeugen
 	struct REFRESH *thread_data = g_malloc(sizeof(struct REFRESH));
+	struct CATCHER *catcher_data = g_malloc(sizeof(struct CATCHER));
+	GAsyncQueue *warteschlange = NULL;
 	gchar *texteingabe = NULL;
-	GThread *thread_id;
 	GtkListStore *store = NULL;
 
 	//übergebene Daten in Thread struct verpacken
 	thread_data->gui = (struct GUI_ELEMENTE*) data;
+	//Asyncrone Queue in die strcut packen
+	warteschlange = g_async_queue_new ();
+	thread_data->queue = warteschlange;
+	catcher_data->queue= warteschlange;
 
 	//Textbuffer auslesen, in Int wandeln und in Thread_daten verpacken
 	texteingabe = gtk_entry_get_text(GTK_ENTRY(thread_data->gui->texteingabe));
@@ -33,46 +46,44 @@ void button_refresh_clicked(GtkWidget *widget, gpointer data)
 	texteingabe = gtk_entry_get_text(GTK_ENTRY(thread_data->gui->texteingabe_spalten_anzahl));
 	thread_data->anzahl_zeilen = g_ascii_strtod (texteingabe,NULL);
 
-	//Wenn anzahl_zeilen = 0, auf eins setzten, damit einen Zeile ausgegeben wird
+	//Wenn anzahl_zeilen = 0, oder eingabe = 0 auf eins setzten, damit einen Zeile ausgegeben wird
 	if (thread_data->anzahl_zeilen<=0) thread_data->anzahl_zeilen=1;
+	if (thread_data->eingabe<=0) thread_data->eingabe=1;
 
-	//Prüfen ob Eingabe != 0 ist, falls ja, die Anzahl an Zeilen anfordern
-	if (thread_data->eingabe!=0){
-		//den Button ausgrauen
-		g_object_set(widget,"sensitive",FALSE,NULL);
-		//Spinner sichbar machen und aktivieren
-		g_object_set(gui_get_spinner(),"visible",TRUE,NULL);
-		gtk_spinner_start (gui_get_spinner());
+	//den Button ausgrauen
+	g_object_set(widget,"sensitive",FALSE,NULL);
 
-		//prüfen ob schon ein Model verbunden ist
-		store = (GtkListStore*)gtk_tree_view_get_model (gui_get_gtk_tree_viewer());
-		if(store!=NULL)
-		{
-			//falls schon ein Model verbunden ist,
-			//müssen wir die Ref() holen, sonst wird die Liste freigegeben, nach der trennung ;)
-			//danach vom viewer trennen
-			//und den Inhalt des Stores löschen
-			g_object_ref(store);
-			gtk_tree_view_set_model (gui_get_gtk_tree_viewer(),NULL);
-			gtk_list_store_clear (store);
-		}
-		else
-		{
-			// ansonsten einen neuen store erzeugen
-			store = gui_get_list_store();
-		}
-		//store in Thread-variable übergeben
-		thread_data->store = store;
-		//thread starten
-		thread_id=g_thread_new("refresh_thread",(GThreadFunc)thread_refresh_button,(gpointer)thread_data);
-		//Timer starten um Thread wieder zu "fangen"
-		g_timeout_add (100,thread_catcher_refresh,(gpointer)thread_id);
+	//Spinner sichbar machen und aktivieren
+	g_object_set(gui_get_spinner(),"visible",TRUE,NULL);
+	gtk_spinner_start (gui_get_spinner());
+
+	//prüfen ob schon ein Model verbunden ist
+	store = (GtkListStore*)gtk_tree_view_get_model (gui_get_gtk_tree_viewer());
+	if(store!=NULL)
+	{
+		//falls schon ein Model verbunden ist,
+		//müssen wir die Ref() holen, sonst wird die Liste freigegeben, nach der trennung ;)
+		//danach vom viewer trennen
+		//und den Inhalt des Stores löschen
+		g_object_ref(store);
+		gtk_tree_view_set_model (gui_get_gtk_tree_viewer(),NULL);
+		gtk_list_store_clear (store);
 	}
-	//Sonst beenden
 	else
 	{
-	g_print("Eingabe war keine Zahle oder 0 ;)\n");
+		// ansonsten einen neuen store erzeugen
+		store = gui_get_list_store();
 	}
+	//store in Thread-variable übergeben
+	thread_data->store = store;
+	//thread starten
+	catcher_data->thread_id=g_thread_new("refresh_thread",(GThreadFunc)thread_refresh_button,(gpointer)thread_data);
+	//Die struct des Threads mitgeben, um sie freizugeben
+	catcher_data->refresh = thread_data;
+	//Timer starten um Thread wieder zu "fangen"
+	g_timeout_add (100,thread_catcher_refresh,(gpointer)catcher_data);
+
+
 	return;
 }
 
@@ -93,9 +104,7 @@ gpointer *thread_refresh_button (gpointer data){
 		thread->anzahl_zeilen--;
 	}
 
-	//Speicher freigeben
-	g_free(thread);
-	thread_status = TRUE;
+	g_async_queue_push(thread->queue,GINT_TO_POINTER(1));
 	g_thread_exit (thread_store);
 	//wird nicht erreicht
 	return (gpointer)thread_store;
@@ -103,16 +112,19 @@ gpointer *thread_refresh_button (gpointer data){
 
 gboolean thread_catcher_refresh (gpointer data){
 	GtkListStore *store=NULL;
+	struct CATCHER *catcher_data = ((struct CATCHER *)data);
+	gint status = 0;
 	//check ob thread fertig
 	//nötig, da die GUI sonst einfriert
-	if (!thread_status)
+	//g_async_queue_try_pop(catcher_data->queue);
+	status = GPOINTER_TO_INT(g_async_queue_try_pop(catcher_data->queue));
+	if (!status)
 	{
 		return TRUE;
 	}
-	//auf Thread warten
-	store = (GtkListStore*)g_thread_join ((GThread*)data);
-	//status = false setzten, solange kein neuer Thread schreibend zugreifen kann
-	thread_status = FALSE;
+	//auf Thread rückgabe einsammeln
+	store = (GtkListStore*)g_thread_join (catcher_data->thread_id);
+
 	//den Button wieder freischalten
 	g_object_set(gui_get_button_refresh(),"sensitive",TRUE,NULL);
 	//Das Model mir dem Viewer verbinden
@@ -122,7 +134,10 @@ gboolean thread_catcher_refresh (gpointer data){
 	gtk_spinner_stop (gui_get_spinner());
 	//store freigeben, er bleibt bestehen, da er mit dem Viewer verknüpft ist
 	g_object_unref(store);
-
+	//Speicher freigeben
+	//erst den Speicher vom Thread freigeben
+	g_free(catcher_data->refresh);
+	g_free(catcher_data);
 	return FALSE;
 }
 
@@ -136,4 +151,3 @@ void buttons_entered (GtkWidget *widget, gpointer data){
   gtk_widget_override_background_color(widget, GTK_STATE_PRELIGHT, &color);
 
 }
-
